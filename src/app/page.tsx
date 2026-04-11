bcaab231f3763cf10643ef6526dbf0a8b2642669
Kristoffer Rolfsson <kristofferrolfsson@MacBook-Pro-som-tillhor-Kristoffer.local>
2026-04-11 12:04:33 +0200

    Search now matches category, description & colour

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -112,7 +112,10 @@ export default function Home() {
         (d) =>
           d.title.toLowerCase().includes(q) ||
           d.store.toLowerCase().includes(q) ||
-          (d.brand && d.brand.toLowerCase().includes(q))
+          (d.brand && d.brand.toLowerCase().includes(q)) ||
+          d.category.toLowerCase().includes(q) ||
+          (d.description && d.description.toLowerCase().includes(q)) ||
+          (d.colour && d.colour.toLowerCase().includes(q))
       );
     }
 
